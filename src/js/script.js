4646fd100601450b032090c34e10ff3bc730058f
Sergei Ordin <seryogaordin@gmail.com>
2022-08-22 13:03:28 +0300

    Added Animation

--- src/js/script.js
+++ src/js/script.js
@@ -85,8 +85,46 @@ $(document).ready(function () {
     validateForms('#consultation form');
     validateForms('#order form');
 
-    //Masks
+    //Mask
 
     $('input[name=phone]').mask('+7 (999) 999-99-99');
+
+    //Form
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'mailer/smart.php',
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find('input').val('');
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('fast');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //Page up btn scroll script
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn('slow');
+        } else {
+            $('.pageup').fadeOut('slow');
+        }
+    })
+
+    $("a[href=#up]").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+
+    // Wow init
+
+    new WOW().init();
 });
 
